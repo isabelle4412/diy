87423157be1d63f99694482bfc3adc04508c4e74
Isabelle Pouget <isab.pouget@gmail.com>
2020-10-30 19:48:28 +0100

    Load products with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
+
   $('#footer').load('../footer.html');
   $('#header').load('../navbar.html');
+
+  $("#all").click(function(){
+    $(".link").removeClass("selected");
+    $(this).addClass("selected");
+    $(".product").hide();
+    $('#product_clothes').load('../vetements.html');
+    $('#product_accessories').load('../accessoires.html');
+    $('#product_toys').load('../jouets.html');
+  });
+
+  $("#clothes").click(function(){
+    $(".link").removeClass("selected");
+    $(this).addClass("selected");
+    $(".product").hide();
+    $('#product_clothes').load('../vetements.html');
+  });
+
+  $("#accessories").click(function(){
+    $(".link").removeClass("selected");
+    $(this).addClass("selected");
+    $(".product").hide();
+    $('#product_accessories').load('../accessoires.html');
+  });
+
+  $("#toys").click(function(){
+      $(".link").removeClass("selected");
+      $(this).addClass("selected");
+      $(".product").hide();
+      $('#product_toys').load('../jouets.html');
+  });
+
 });
